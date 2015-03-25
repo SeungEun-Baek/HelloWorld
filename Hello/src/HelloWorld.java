b055c0fa4dba0d7fffd78f7aebb81bfdf4be9177
sebaeknate <b0s0e0@nate.com>
2015-03-25 10:53:25 +0900

    reminder operation

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -5,7 +5,17 @@ public class HelloWorld {
 
 		System.out.println("Hello World");
 		System.out.println("Hello World2");
+		System.out.println((-10) % (-8));
+		System.out.println((-10) % (-9));
+		System.out.println((-10) % (-10));
+		System.out.println((-10) % (-11));
 		
+		System.out.println();
+		
+		System.out.println((-10) % (8));
+		System.out.println((-10) % (9));
+		System.out.println((-10) % (10));
+		System.out.println((-10) % (11));
 		
 		
 		
